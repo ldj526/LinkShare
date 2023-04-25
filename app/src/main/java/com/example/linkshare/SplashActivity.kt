49d5abd165ca43a9641ef347d948ef59fdbd901c
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-04-25 20:47:13 +0900

    feat: Splash 기능 추가

---
+++ app/src/main/java/com/example/linkshare/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.example.linkshare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import timber.log.Timber
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Timber.d("onCreate")
+    }
+}
