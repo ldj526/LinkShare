c6e324bc7fd6e2119bc77d4de95febfea1a769ff
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-04-26 20:45:55 +0900

    feat: 로그인 화면 구성

--- app/src/main/java/com/example/linkshare/SplashActivity.kt
+++ app/src/main/java/com/example/linkshare/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.example.linkshare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.linkshare.auth.IntroActivity
 import timber.log.Timber
 
 class SplashActivity : AppCompatActivity() {
@@ -12,6 +14,10 @@ class SplashActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val intent = Intent(this, IntroActivity::class.java)
+        startActivity(intent)
+        finish()
 
         Timber.d("onCreate")
     }
