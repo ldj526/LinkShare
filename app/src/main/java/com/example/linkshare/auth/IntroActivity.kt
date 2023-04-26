489eafb1bcfa26051434ab6c7f516f750babc49b
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-04-26 21:57:55 +0900

    feat: 회원가입 화면 구성

--- app/src/main/java/com/example/linkshare/auth/IntroActivity.kt
+++ app/src/main/java/com/example/linkshare/auth/IntroActivity.kt
@@ -1,12 +1,23 @@
 package com.example.linkshare.auth
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.linkshare.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.linkshare.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.joinText.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
