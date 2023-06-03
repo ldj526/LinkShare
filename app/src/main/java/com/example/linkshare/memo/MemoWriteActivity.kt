019c747ea1d5f5503186bbbd983970adf7f89296
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-06-03 21:22:09 +0900

    feat: 메모에 이미지 넣는 기능 추가

--- app/src/main/java/com/example/linkshare/memo/MemoWriteActivity.kt
+++ app/src/main/java/com/example/linkshare/memo/MemoWriteActivity.kt
@@ -1,6 +1,8 @@
 package com.example.linkshare.memo
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
 import com.example.linkshare.databinding.ActivityMemoWriteBinding
 import com.example.linkshare.utils.FBAuth
@@ -16,6 +18,11 @@ class MemoWriteActivity : AppCompatActivity() {
         _binding = ActivityMemoWriteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.imageView.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, 100)
+        }
+
         binding.writeBtn.setOnClickListener {
             val title = binding.title.text.toString()
             val content = binding.content.text.toString()
@@ -25,6 +32,13 @@ class MemoWriteActivity : AppCompatActivity() {
             FBRef.memoList.push().setValue(MemoModel(title, content, uid, time))
 
             finish()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == 100) {
+            binding.imageView.setImageURI(data?.data)
         }
     }
 
