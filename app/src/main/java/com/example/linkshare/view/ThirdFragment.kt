53e44870bc2530ac6776753baee1df9e99adcd17
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-06-06 23:54:48 +0900

    feat: Logout 기능 구현

--- app/src/main/java/com/example/linkshare/view/ThirdFragment.kt
+++ app/src/main/java/com/example/linkshare/view/ThirdFragment.kt
@@ -1,13 +1,28 @@
 package com.example.linkshare.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.example.linkshare.R
+import com.example.linkshare.auth.IntroActivity
+import com.example.linkshare.databinding.FragmentMemolistBinding
+import com.example.linkshare.databinding.FragmentThirdBinding
+import com.example.linkshare.utils.FBRef
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ThirdFragment : Fragment() {
+
+    private var _binding: FragmentThirdBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
+    private var alertDialog: AlertDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +33,42 @@ class ThirdFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false)
+        _binding = FragmentThirdBinding.inflate(inflater, container, false)
+
+        auth = Firebase.auth
+
+        binding.logout.setOnClickListener {
+            showDialog()
+        }
+
+        return binding.root
+    }
+
+    private fun showDialog() {
+        val mDialogView = LayoutInflater.from(context).inflate(R.layout.custom_dialog, null)
+        val mBuilder =
+            AlertDialog.Builder(requireContext()).setView(mDialogView).setTitle("로그아웃 하시겠습니까?")
+        alertDialog = mBuilder.show()
+
+        alertDialog!!.findViewById<Button>(R.id.removeBtn)?.setOnClickListener {
+            auth.signOut()
+            val intent = Intent(context, IntroActivity::class.java)
+            intent.flags =
+                Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+
+        alertDialog!!.findViewById<Button>(R.id.cancelBtn)?.setOnClickListener {
+            alertDialog!!.dismiss()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // Dialog가 fragment 종료 후에도 남아있는 것을 방지
+        if (alertDialog != null && alertDialog!!.isShowing) {
+            alertDialog!!.dismiss()
+        }
+        _binding = null
     }
 }
