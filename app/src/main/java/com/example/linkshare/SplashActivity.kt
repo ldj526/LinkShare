9e6993a8082ff8a971ce4df62242cb12889b20c3
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-06-06 14:42:44 +0900

    feat: 앱 실행 시 로그인/로그아웃 상태에 따른 초기화면 설정

--- app/src/main/java/com/example/linkshare/SplashActivity.kt
+++ app/src/main/java/com/example/linkshare/SplashActivity.kt
@@ -1,13 +1,19 @@
 package com.example.linkshare
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.linkshare.auth.IntroActivity
+import com.example.linkshare.view.MainActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import timber.log.Timber
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
 
         installSplashScreen()
@@ -15,9 +21,17 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        val intent = Intent(this, IntroActivity::class.java)
-        startActivity(intent)
-        finish()
+        auth = Firebase.auth
+
+        if (auth.currentUser?.uid == null) {
+            val intent = Intent(this, IntroActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
         Timber.d("onCreate")
     }
