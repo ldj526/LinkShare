1976d0d7960a0aaaea82812ad5a52aa847c6f091
ldj526 <59276421+ldj526@users.noreply.github.com>
2024-01-08 21:59:47 +0900

    feat: Splash 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.linkshare"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.linkshare"
@@ -36,6 +36,9 @@ android {
 }
 
 dependencies {
+
+    // Splash
+    implementation("androidx.core:core-splashscreen:1.0.0")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
