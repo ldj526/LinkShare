7eb117890ecd9adacf3ba5f49e85aae03dd91aab
ldj526 <59276421+ldj526@users.noreply.github.com>
2024-01-13 23:36:53 +0900

    feat: Memo 작성 화면 추가

---
+++ app/src/main/java/com/example/linkshare/memo/MemoWrite.kt
@@ -0,0 +1,12 @@
+package com.example.linkshare.memo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.linkshare.R
+
+class MemoWrite : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memo_write)
+    }
+}
