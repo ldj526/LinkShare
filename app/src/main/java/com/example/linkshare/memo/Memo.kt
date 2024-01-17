fca559e120dcfb399c6dcef936ca64833d1cb3bc
ldj526 <59276421+ldj526@users.noreply.github.com>
2024-01-17 21:56:39 +0900

    feat: 메모 Database에 저장 및 RecyclerView에 연결

---
+++ app/src/main/java/com/example/linkshare/memo/Memo.kt
@@ -0,0 +1,9 @@
+package com.example.linkshare.memo
+
+data class Memo(
+    val title: String? = "",
+    val content: String? = "",
+    val link: String? = "",
+    val uid: String? = "",
+    val time: String? = ""
+)
--- app/src/main/java/com/example/linkshare/memo/MemoWrite.kt
+++ app/src/main/java/com/example/linkshare/memo/MemoWrite.kt
@@ -1,12 +1,30 @@
 package com.example.linkshare.memo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.linkshare.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.linkshare.databinding.ActivityMemoWriteBinding
+import com.example.linkshare.util.FBAuth
+import com.example.linkshare.util.FBRef
 
 class MemoWrite : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMemoWriteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_memo_write)
+        binding = ActivityMemoWriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSave.setOnClickListener {
+            val title = binding.etTitle.text.toString()
+            val content = binding.etContent.text.toString()
+            val link = binding.etLink.text.toString()
+            val uid = FBAuth.getUid()
+            val time = FBAuth.getTime()
+
+            // Firebase database에 추가
+            FBRef.memoCategory.push().setValue(Memo(title, content, link, uid, time))
+            finish()
+        }
     }
 }
