5cf022476809498c2c4417ba1920b14ee47cfc86
ldj526 <59276421+ldj526@users.noreply.github.com>
2024-01-21 23:31:51 +0900

    feat: LongClick으로 지도에 마커 찍고 Toast Message 띄우기

--- app/src/main/java/com/example/linkshare/view/MapViewActivity.kt
+++ app/src/main/java/com/example/linkshare/view/MapViewActivity.kt
@@ -1,21 +1,28 @@
 package com.example.linkshare.view
 
+import android.location.Geocoder
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.linkshare.R
 import com.example.linkshare.databinding.ActivityMapViewBinding
 import com.google.firebase.annotations.concurrent.UiThread
+import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
+import java.util.Locale
 
 class MapViewActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityMapViewBinding
     private lateinit var locationSource: FusedLocationSource
     private lateinit var naverMap: NaverMap
+    private val marker = Marker()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,6 +61,45 @@ class MapViewActivity : AppCompatActivity(), OnMapReadyCallback {
         // 현재 위치 버튼 기능
         naverMap.uiSettings.isLocationButtonEnabled = true
         naverMap.uiSettings.isZoomControlEnabled = false
+
+        naverMap.setOnMapLongClickListener { pointF, latLng ->
+            marker(latLng.latitude, latLng.longitude)
+        }
+    }
+
+    // 마커 생성
+    private fun marker(latitude: Double, longitude: Double) {
+        marker.position = LatLng(latitude, longitude)
+        marker.map = naverMap
+
+        getAddress(latitude, longitude)
+    }
+
+    // Geocoder로 주소 가져오기
+    private fun getAddress(latitude: Double, longitude: Double) {
+        val geocoder = Geocoder(applicationContext, Locale.KOREAN)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            geocoder.getFromLocation(
+                latitude, longitude, 1
+            ) { address ->
+                if (address.size != 0) {
+                    toast(address[0].getAddressLine(0))
+                }
+            }
+        } else {
+            val addresses = geocoder.getFromLocation(latitude, longitude, 1)
+            if (addresses != null) {
+                toast(addresses[0].getAddressLine(0))
+            }
+        }
+    }
+
+    // Toast Message
+    private fun toast(text: String) {
+        runOnUiThread {
+            Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).show()
+        }
     }
 
     companion object {
