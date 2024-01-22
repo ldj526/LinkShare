fe29ed6989b19f995cec84de60ac61fb0c1a5fab
ldj526 <59276421+ldj526@users.noreply.github.com>
2024-01-22 21:57:28 +0900

    feat: 검색해서 나온 결과 클릭 시 해당 장소 마커, 카메라 이동 기능

--- app/src/main/java/com/example/linkshare/util/LocalInfo.kt
+++ app/src/main/java/com/example/linkshare/util/LocalInfo.kt
@@ -2,5 +2,7 @@ package com.example.linkshare.util
 
 data class LocalInfo (
     val title: String,
-    val roadAddress: String
+    val roadAddress: String,
+    val mapx: Int,
+    val mapy: Int
 )
