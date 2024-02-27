fafbc10d81a17c5572f667a1916e8234f40800a3
ldj526 <59276421+ldj526@users.noreply.github.com>
2024-02-27 19:05:54 +0900

    feat: MemoActivity와 MapViewActivity간 데이터 전달 기능
    Map에서 선택한 곳을 Memo에 전달해주고, Memo에서 Map으로 이동할 때에는 Memo에 표시된 장소로 이동시켜주는 기능

--- app/src/main/java/com/example/linkshare/memo/Memo.kt
+++ app/src/main/java/com/example/linkshare/memo/Memo.kt
@@ -5,6 +5,8 @@ data class Memo(
     val content: String = "",
     val link: String = "",
     val location: String? = "",
+    val latitude: Double? = 0.0,
+    val longitude: Double? = 0.0,
     val uid: String = "",
     val time: String = ""
 )
