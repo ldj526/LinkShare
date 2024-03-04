8082ac24971636e22fbf323e094bb2d3ba016e2d
ldj526 <59276421+ldj526@users.noreply.github.com>
2024-03-05 00:49:15 +0900

    feat: 게시글에 댓글 추가하는 기능

---
+++ app/src/main/java/com/example/linkshare/comment/CommentRVAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.linkshare.comment
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.linkshare.R
+import com.example.linkshare.util.FBAuth
+
+class CommentRVAdapter(val commentList: MutableList<Comment>) :
+    RecyclerView.Adapter<CommentRVAdapter.ViewHolder>() {
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title = itemView.findViewById<TextView>(R.id.tv_comment_title)
+        val time = itemView.findViewById<TextView>(R.id.tv_comment_time)
+        val delete = itemView.findViewById<ImageView>(R.id.iv_comment_delete)
+
+        fun bind(comment: Comment) {
+            title.text = comment.comment
+            time.text = comment.time
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.comment_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = commentList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (commentList[position].uid == FBAuth.getUid()) {
+            holder.delete.visibility = View.VISIBLE
+        }
+        holder.bind(commentList[position])
+    }
+}
