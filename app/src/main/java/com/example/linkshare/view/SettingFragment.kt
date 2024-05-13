c0ac0e2362e2384a359a231d9e644c062e0e7ea6
ldj526 <59276421+ldj526@users.noreply.github.com>
2024-05-13 21:54:06 +0900

    feat: 로그인 계정 정보 추가

--- app/src/main/java/com/example/linkshare/view/SettingFragment.kt
+++ app/src/main/java/com/example/linkshare/view/SettingFragment.kt
@@ -5,13 +5,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.linkshare.auth.IntroActivity
 import com.example.linkshare.databinding.FragmentSettingBinding
 import com.example.linkshare.util.CustomDialog
 import com.google.firebase.Firebase
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.auth
+import com.kakao.sdk.user.UserApiClient
 
 class SettingFragment : Fragment() {
 
@@ -31,6 +36,55 @@ class SettingFragment : Fragment() {
             showLogoutDialog()
         }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val user = FirebaseAuth.getInstance().currentUser
+
+        if (user != null) {
+            val email = user.email
+            displayAuthenticationMethod(user)
+            updateProfileUI(email)
+        } else {
+            updateProfileUI(null)
+        }
+
+        fetchKakaoUserEmail()
+    }
+
+    // 카카오 계정 이메일 가져오기
+    private fun fetchKakaoUserEmail() {
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                Toast.makeText(requireContext(), "사용자 정보 요청 실패", Toast.LENGTH_SHORT).show()
+            } else {
+                val email = user?.kakaoAccount?.email
+                updateProfileUI(email)
+            }
+        }
+    }
+
+    // 로그인한 플랫폼 표시
+    private fun displayAuthenticationMethod(user: FirebaseUser) {
+        user.providerData.forEach { profile ->
+            val method = when (profile.providerId) {
+                EmailAuthProvider.PROVIDER_ID -> "이메일 계정"
+                GoogleAuthProvider.PROVIDER_ID -> "구글 계정"
+                else -> "카카오 계정"
+            }
+            binding.tvLoginMethod.text = method
+        }
+    }
+
+    // 이메일 표시
+    private fun updateProfileUI(email: String?) {
+        if (email != null) {
+            binding.tvProfileEmail.text = email
+        } else {
+            binding.tvProfileEmail.text = "이메일 정보 없음"
+        }
     }
 
     private fun showLogoutDialog() {
